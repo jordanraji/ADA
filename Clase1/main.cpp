77967cca51f3ae8e2137f00e24e80301ea061f08
Jordan Raji Lazo Cahua <jordanraji@gmail.com>
2016-03-23 19:45:24 -0500

    MERGE

--- Clase1/main.cpp
+++ Clase1/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <vector>
 
 using namespace std;
@@ -19,14 +19,48 @@ vector<int> insertsort(vector<int> &A){
     return A;
 }
 
+vector<int> merge(vector<int> &A , int p , int q , int r){
+    int n1=q-p+1;
+    int n2=r-q;
+    vector<int> L,R;
+    for(int i=1;i<=n1;i++){
+        L.push_back(A[p+i-1]);
+    }
+    for (int j=1; j<=n2; j++) {
+        R.push_back(A[q+j]);
+    }
+    L.push_back(1000000000);
+    R.push_back(1000000000);
+    int i=0,j=0;
+    for (int k=p; k<=r; k++) {
+        if (L[i]<=R[j]) {
+            A[k]=L[i];
+            i++;
+        } else {
+            A[k]=R[j];
+            j++;
+        }
+    }
+    return A;
+}
+
+void mergeSort(vector<int> &A, int p, int r){
+    int q;
+    if (p<r) {
+        q=(p+r)/2;
+        mergeSort(A, p, q);
+        mergeSort(A, q+1, r);
+        merge(A,p,q,r);
+    }
+}
+
 void mostrar(vector <int> A){
     for(int i=0; i<A.size(); i++){
         cout<<A[i]<<endl;
     }
 }
 
-int main()
-{
+int main(int argc, const char * argv[]) {
     vector<int> lista;
     lista.push_back(31);
     lista.push_back(25);
@@ -34,10 +68,13 @@ int main()
     lista.push_back(3000);
     lista.push_back(10);
     lista.push_back(8);
+//    mostrar(lista);
+//    insertsort(lista);
+//    cout<<endl<<endl;
+//    mostrar(lista);
+//    cout<<"Hola, mundo!"<<endl;
+    mergeSort(lista, 0, 5);
     mostrar(lista);
-    insertsort(lista);
-    cout<<endl<<endl;
-    mostrar(lista);
-    cout << "Hello world!" << endl;
+    
     return 0;
 }
